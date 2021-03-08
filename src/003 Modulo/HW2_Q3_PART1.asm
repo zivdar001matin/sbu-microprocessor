38bde5c40151934df499aae27de5031bfbd233ba
Matin <zivdar1matin@gmail.com>
2021-03-08 13:56:31 +0330

    Print result

--- src/003 Modulo/HW2_Q3_PART1.asm
+++ src/003 Modulo/HW2_Q3_PART1.asm
@@ -40,7 +40,12 @@ MAIN    PROC    FAR
         MOV     DX, OFFSET MSG3
         MOV     AH, 9
         INT     21h
-        ;TODO
+        MOV     SI, OFFSET NUMSTR
+        MOV     AX, RESULT
+        CALL    NUMBER2STRING   ;retruns numstr
+        MOV     AH, 9           ;display string
+        MOV     DX, OFFSET NUMSTR
+        INT     21h
         ;
         MOV     AH, 4CH     ;set up to
         INT     21H
@@ -289,4 +294,54 @@ make_minus_b    DB      ?       ; used as a flag.
 two             DW      2      ; used as multiplier/divider by SCAN_NUM & PRINT_NUM_UNS.
 SCAN_BNUM       ENDP
 ;----------------------------------------------------------
+;SOURCE: https://stackoverflow.com/questions/37605815/how-can-i-print-0-to-100-in-assembly-language-in-emu-8086
+;----------------------------------------------------------
+;CONVERT A NUMBER IN STRING.
+;ALGORITHM : EXTRACT DIGITS ONE BY ONE, STORE
+;THEM IN STACK, THEN EXTRACT THEM IN REVERSE
+;ORDER TO CONSTRUCT STRING (STR).
+;PARAMETERS : AX = NUMBER TO CONVERT.
+;             SI = POINTING WHERE TO STORE STRING.
+
+number2string proc 
+  call dollars ;FILL STRING WITH $.
+  mov  bx, 10  ;DIGITS ARE EXTRACTED DIVIDING BY 10.
+  mov  cx, 0   ;COUNTER FOR EXTRACTED DIGITS.
+cycle1:       
+  mov  dx, 0   ;NECESSARY TO DIVIDE BY BX.
+  div  bx      ;DX:AX / 10 = AX:QUOTIENT DX:REMAINDER.
+  push dx      ;PRESERVE DIGIT EXTRACTED FOR LATER.
+  inc  cx      ;INCREASE COUNTER FOR EVERY DIGIT EXTRACTED.
+  cmp  ax, 0   ;IF NUMBER IS
+  jne  cycle1  ;NOT ZERO, LOOP. 
+;NOW RETRIEVE PUSHED DIGITS.
+cycle2:  
+  pop  dx        
+  add  dl, 48  ;CONVERT DIGIT TO CHARACTER.
+  mov  [ si ], dl
+  inc  si
+  loop cycle2  
+
+  ret
+number2string endp       
+
+;------------------------------------------
+;FILLS VARIABLE WITH '$'.
+;USED BEFORE CONVERT NUMBERS TO STRING, BECAUSE
+;THE STRING WILL BE DISPLAYED.
+;PARAMETER : SI = POINTING TO STRING TO FILL.
+
+proc dollars                 
+  mov  cx, 5
+  mov  di, offset numstr
+dollars_loop:      
+  mov  bl, '$'
+  mov  [ di ], bl
+  inc  di
+  loop dollars_loop
+
+  ret
+endp  
+
+;------------------------------------------
         END MAIN            ;this is the program exit point         
