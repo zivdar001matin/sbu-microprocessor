85989165b17e8dc77dc50221199517228fe68109
Matin Zivdar <zivdar1matin@gmail.com>
2021-05-17 23:02:54 +0430

    Fix GetKeyPress function

--- src/012 Elevator/Keil/Elevator.s
+++ src/012 Elevator/Keil/Elevator.s
@@ -24,6 +24,11 @@ SystemInit FUNCTION
 	LDR R1, =GPIOA_MODER
 	MOV R0, 0x55555555
 	STR R0, [R1]
+	;GPIOB MODER 
+	LDR R1 , =GPIOB_MODER
+	LDR R0 , [R1]
+	MOV R0 , 0x55000000 ;01 01 01 01 -> 00000 
+	STR R0 , [R1]
 	
  ENDFUNC
 
@@ -42,8 +47,8 @@ GetKeyPress FUNCTION
 ;		R0, R1, R2, R3
 	;FIRST ROW KP ENABLE
 	LDR R2 , =GPIOB_ODR
-	MOV R0 , 0xEFFF
-	STR R0 , [R2]
+	MOV R3 , 0xEFFF
+	STR R3 , [R2]
 	
 	LDR R2 , =GPIOB_IDR
 	LDR R0 , [R2]
@@ -51,80 +56,104 @@ GetKeyPress FUNCTION
 
 	;check if 1 is pressed
 	MOV R3 , 0xFEFF ;value for key 1 -> 1111 1110 1111 1111 -> kp1 ->0
-	MOV R1, R0
+	MOV R0, R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ ONE_PRESSED
 	;check if 2 is pressed
 	MOV R3 , 0xFDFF ;value for key 2 -> 1111 1101 1111 1111 -> kp2 ->0
 	MOV R0 , R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ TWO_PRESSED
 	;check if 3 is pressed
 	MOV R3 , 0xFBFF ;value for key 3 -> 1111 1011 1111 1111 -> kp3 ->0
 	MOV R0 , R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ THREE_PRESSED
 	
 	;SECOND ROW KP ENABLE
 	LDR R2 , =GPIOB_ODR
-	MOV R0 , 0xDFFF
-	STR R0 , [R2]
+	MOV R3 , 0xDFFF
+	STR R3 , [R2]
+
+	LDR R2 , =GPIOB_IDR
+	LDR R0 , [R2]
+	MOV R1 , R0
 
 	;check if 4 is pressed
 	MOV R3 , 0xFEFF ;value for key 4 -> 1111 1110 1111 1111 -> kp1 ->0
-	MOV R1, R0
+	MOV R0, R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ FOUR_PRESSED
 	;check if 5 is pressed
 	MOV R3 , 0xFDFF ;value for key 5 -> 1111 1101 1111 1111 -> kp2 ->0
 	MOV R0 , R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ FIVE_PRESSED
 	;check if 6 is pressed
 	MOV R3 , 0xFBFF ;value for key 6 -> 1111 1011 1111 1111 -> kp3 ->0
 	MOV R0 , R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ SIX_PRESSED
 	
 	;THIRD ROW KP ENABLE
 	LDR R2 , =GPIOB_ODR
-	MOV R0 , 0xBFFF
-	STR R0 , [R2]
+	MOV R3 , 0xBFFF
+	STR R3 , [R2]
+
+	LDR R2 , =GPIOB_IDR
+	LDR R0 , [R2]
+	MOV R1 , R0
 
 	;check if 7 is pressed
 	MOV R3 , 0xFEFF ;value for key 7 -> 1111 1110 1111 1111 -> kp1 ->0
-	MOV R1, R0
+	MOV R0, R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ SEVEN_PRESSED
 	;check if 8 is pressed
 	MOV R3 , 0xFDFF ;value for key 8 -> 1111 1101 1111 1111 -> kp2 ->0
 	MOV R0 , R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ EIGHT_PRESSED
 	;check if 9 is pressed
 	MOV R3 , 0xFBFF ;value for key 9 -> 1111 1011 1111 1111 -> kp3 ->0
 	MOV R0 , R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ NINE_PRESSED
 	
 	;FOURTH ROW KP ENABLE
 	LDR R2 , =GPIOB_ODR
-	MOV R0 , 0x7FFF
-	STR R0 , [R2]
+	MOV R3 , 0x7FFF
+	STR R3 , [R2]
+
+	LDR R2 , =GPIOB_IDR
+	LDR R0 , [R2]
+	MOV R1 , R0
 
 	;check if 7 is pressed
 	MOV R3 , 0xFEFF ;value for key * -> 1111 1110 1111 1111 -> kp1 ->0
-	MOV R1, R0
+	MOV R0, R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ STAR_PRESSED
 	;check if 8 is pressed
 	MOV R3 , 0xFDFF ;value for key 0 -> 1111 1101 1111 1111 -> kp2 ->0
 	MOV R0 , R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ ZERO_PRESSED
 	;check if 9 is pressed
 	MOV R3 , 0xFBFF ;value for key # -> 1111 1011 1111 1111 -> kp3 ->0
 	MOV R0 , R1
+	ORR R0, R0, R3
 	CMP R0 , R3
 	BEQ HASH_PRESSED
 
