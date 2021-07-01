39ad25c2d61829591146247dd7511e99c8a62b6b
Matin Zivdar <zivdar1matin@gmail.com>
2021-07-02 00:16:41 +0430

    Trying to use GLCD ...

--- src/014 Oscilloscope/KeilDisplayUnit/DisplayUnit.c
+++ src/014 Oscilloscope/KeilDisplayUnit/DisplayUnit.c
@@ -12,6 +12,16 @@ void delay10Us(volatile uint32_t num);
 uint32_t USART2_read(void);
 void USART2_write(uint32_t data);
 
+//GLCD Function
+void GLCD_on(void);
+void GLCD_off(void);
+void GLCD_putImage(unsigned char data,unsigned char j);
+void GLCD_puts_point(char x,char y,char w);
+void GLCD_disp_Image(unsigned char *str,char x1,char x2, char y1,char y2);
+void GLCD_gotoxy(unsigned char x, unsigned char y);
+void GLCD_clear_all(void);
+void GLCD_command(uint32_t com);
+
 int main(){
 	INIT_PORTA();
 	INIT_PORTB();
@@ -21,12 +31,16 @@ int main(){
 	// Clear USART data register
 	USART2->DR;
 
+	// GLCD_on();
+
 	while(1){
 		// Receive Packet
 		uint32_t packet[8], i;
 		for (i = 0; i < 8; i++) {
 			packet[i] = USART2_read();
 		}
+
+		// GLCD_putImage(0xF0, 0x4);
 
 		volatile static uint32_t b_fixed;
 		volatile static uint32_t b_floating;
@@ -45,6 +59,82 @@ int main(){
 		//TODO show on GLCD
 
 		USART2_write(1); 	// Send finish acknowledgment
+	}
+}
+
+void GLCD_ready(void) {
+	volatile uint32_t status;
+	GPIOC->MODER = 0x55550000; 	/* make GPIOC as input pins */
+	GPIOC->ODR = 0x100 ; 				/* RS = 0, R/W = 1, LCD output*/
+	do { 												/* stay in the loop until it is not busy */
+		GPIOC->ODR |= 0x400; 			/* pulse E */
+		delay(1);
+		status = GPIOC->IDR; 			/* read status register */
+		GPIOC->ODR &= 0xBFF; 			/* clear pulse E */
+		delay(1); 								
+	} while (status & 0x80); 		/* check busy bit */
+	
+}
+
+void GLCD_command(uint32_t com) {
+	GLCD_ready();
+	GPIOC->ODR = 0x000 | com; 	/* RS = 0, R/W = 0, write command*/
+	GPIOC->ODR |= 0x400; 				/* pulse E */
+	delay(1);
+	GPIOC->ODR &= 0xBFF; 				/* clear pulse E */
+}
+
+void GLCD_on(void){
+	delay(1);
+	GLCD_command(0x3F);     //GLCD on
+	GLCD_command(0x40);     //SET Cursor on Y=0
+	GLCD_command(0xB8);     //SET Page on X=0,Line=0
+	GLCD_command(0xC0);     //Display Start Line=0xC0
+}
+
+void GLCD_off(void){
+	delay(1);
+	GLCD_command(0x3E);
+}
+
+void GLCD_putImage(unsigned char data,unsigned char j){
+	if(j<64){                                      //Left section
+	GPIOC->ODR &= ~0x400;						   //E_PIN_NUMBER=RESET
+		delay(1);
+	GPIOC->ODR &= ~0x100;							//RW_PIN_NUMBER=RESET
+		delay(1);
+	GPIOC->ODR |= 0x200;				          //DI(RS)_PIN_NUMBER=SET
+		delay(1);
+	GPIOC->ODR |= 0x800;					         //CS1_PIN_NUMBER=SET
+		delay(1);
+	GPIOC->ODR &= ~0xF00;							//CS2_PIN_NUMBER=RESET
+		delay(1);
+  	GPIOC->ODR = 0x00;							//DATA_PORT=RESET
+		delay(1);
+	GPIOC->ODR |= 0x400;           //E_PIN_NUMBER=SET
+		delay(1);
+	GPIOC->ODR = data;       //DATA_PORT=DATA
+		delay(1);
+	GPIOC->ODR &= ~ 0x400;       //E_PIN_NUMBER=RESET
+	}                                              
+	else{                                          //Right section
+	GPIOC->ODR &= ~ 0x400;       //E_PIN_NUMBER=RESET
+		delay(1);
+	GPIOC->ODR &= ~ 0x100;     //RW_PIN_NUMBER=RESET
+		delay(1);
+	GPIOC->ODR |= 0x200;         //DI_PIN_NUMBER=SET
+		delay(1);
+	GPIOC->ODR &= ~ 0x800;    //CS1_PIN_NUMBER=RESET
+		delay(1);
+	GPIOC->ODR |= 0xF00;        //CS2_PIN_NUMBER=SET
+		delay(1);
+  GPIOC->ODR = 0x00;			  //DATA_PORT=RESET
+		delay(1);
+	GPIOC->ODR |= 0x400;          //E_PIN_NUMBER=SET
+		delay(1);
+	GPIOC->ODR = data;       //DATA_PORT=DATA
+		delay(1);
+	GPIOC->ODR &= ~ 0x400;      //E_PIN_NUMBER=RESET
 	}
 }
 
